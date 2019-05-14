e3992f464f639ccdba1c7db8b94a8e1d85b2016a
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-14 22:52:34 +0300

    Add form-backing bean

---
+++ NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/form/AddNewNetworkFormBean.java
@@ -0,0 +1,10 @@
+package neuralnetwork.trainerweb.bean.form;
+
+/**
+ * Bean to be populated with values the user entered in the 'Add new network' form
+ * and to be used to create a concrete instance of network or load it from a file.
+ * @author Konstantin Zhdanov
+ */
+public class AddNewNetworkFormBean {
+    
+}

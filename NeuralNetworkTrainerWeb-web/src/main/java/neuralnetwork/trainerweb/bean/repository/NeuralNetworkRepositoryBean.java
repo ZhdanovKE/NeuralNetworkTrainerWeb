513d6d11133155c87ab3d41bbd5d889ed5c8f48b
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-15 15:48:46 +0300

    Make Serializable to be used in beans with different scopes

--- NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/repository/NeuralNetworkRepositoryBean.java
+++ NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/repository/NeuralNetworkRepositoryBean.java
@@ -1,17 +1,21 @@
 package neuralnetwork.trainerweb.bean.repository;
 
+import java.io.Serializable;
 import javax.enterprise.context.Dependent;
 import neuralnetwork.NamedNeuralNetwork;
 import neuralnetwork.commons.repository.NamedObjectRepository;
 
 /**
- * CDI-managed repository Bean that provides access to the available 
+ * CDI-managed repository bean that provides access to the available 
  * Neural Networks.
  * @author Konstantin Zhdanov
  */
 @Dependent
-public class NeuralNetworkRepositoryBean extends NamedObjectRepository<NamedNeuralNetwork>{
+public class NeuralNetworkRepositoryBean extends NamedObjectRepository<NamedNeuralNetwork> 
+        implements Serializable{
     
+    private static final long serialVersionUID = -754029538755690090L;
+     
     public NeuralNetworkRepositoryBean() {
         super.add("Network 1", new NamedNeuralNetwork(1, new int[]{2, 3}, 4, "Network 1"));
         super.add("Network 2", new NamedNeuralNetwork(2, new int[]{3, 4}, 5, "Network 2"));

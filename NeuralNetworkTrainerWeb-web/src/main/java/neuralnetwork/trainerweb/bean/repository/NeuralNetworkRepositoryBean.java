94a5f75046d516297032bbdeccc2e53ba68c32c3
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-21 15:24:30 +0300

    Switch to use EJB JPA facade to perform repository's operations
    *Inject Local View of EJB.
    
    *Remove network's creation from constructor - no longer needed, networks
    are read at load-time.
    
    *Make 'containsObject' and 'getNameForObject' unsupported operations
    (for now)  as they require traversal of all database, which is costly.

--- NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/repository/NeuralNetworkRepositoryBean.java
+++ NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/repository/NeuralNetworkRepositoryBean.java
@@ -1,9 +1,14 @@
 package neuralnetwork.trainerweb.bean.repository;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import neuralnetwork.NamedNeuralNetwork;
 import neuralnetwork.commons.repository.NamedObjectRepository;
+import neuralnetwork.jpa.entity.NeuralNetworkEntity;
+import neuralnetwork.trainerweb.ejb.NeuralNetworkFacadeLocal;
 
 /**
  * CDI-managed repository bean that provides access to the available 
@@ -13,12 +18,75 @@ import neuralnetwork.commons.repository.NamedObjectRepository;
 @SessionScoped
 public class NeuralNetworkRepositoryBean extends NamedObjectRepository<NamedNeuralNetwork> 
         implements Serializable{
-    
+            
     private static final long serialVersionUID = -754029538755690090L;
+    
+    @EJB
+    private NeuralNetworkFacadeLocal nnEJBFacade;
      
     public NeuralNetworkRepositoryBean() {
-        super.add("Network 1", new NamedNeuralNetwork(1, new int[]{2, 3}, 4, "Network 1"));
-        super.add("Network 2", new NamedNeuralNetwork(2, new int[]{3, 4}, 5, "Network 2"));
-        super.add("Network 3", new NamedNeuralNetwork(3, new int[]{4, 5}, 6, "Network 3"));
     }
+
+    @Override
+    public void add(String name, NamedNeuralNetwork object) {
+        object.setName(name);
+        nnEJBFacade.persist(object);
+    }
+
+    @Override
+    public boolean remove(String name) {
+        nnEJBFacade.remove(name);
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return nnEJBFacade.count();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    
+    @Override
+    public boolean containsName(String name) {
+        return nnEJBFacade.findByName(name) != null;
+    }
+
+    @Override
+    public NamedNeuralNetwork get(String name) {
+        NeuralNetworkEntity entity = nnEJBFacade.findByName(name);
+        return entity == null ? null : entity.getNetwork();
+    }
+    
+    @Override
+    public void rename(String oldName, String newName) {
+        NeuralNetworkEntity entity = nnEJBFacade.findByName(oldName);
+        entity.setName(newName);
+        nnEJBFacade.merge(entity);
+        super.onNameChange(entity.getNetwork(), oldName, newName);
+    }
+
+    @Override
+    public boolean containsObject(NamedNeuralNetwork object) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getNameForObject(NamedNeuralNetwork object) {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public List<String> getNamesList() {
+        return nnEJBFacade.findAll().stream().
+                map(NamedNeuralNetwork::getName).
+                collect(Collectors.toList());
+    }
+
+    @Override
+    public List<NamedNeuralNetwork> getObjectsList() {
+        return nnEJBFacade.findAll();
+    } 
 }

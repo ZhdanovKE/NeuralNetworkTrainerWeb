12766e2b1d8ecbb2f86b1f483963c18fe5182278
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-25 23:26:28 +0300

    Switch to using EJB facade bean for accessing samples repositories
    *Make methods for getting names for the objects and checking existence
    of objects unsupported.

--- NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/repository/SamplesRepositoryBean.java
+++ NeuralNetworkTrainerWeb-web/src/main/java/neuralnetwork/trainerweb/bean/repository/SamplesRepositoryBean.java
@@ -1,10 +1,14 @@
 package neuralnetwork.trainerweb.bean.repository;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
 import javax.enterprise.context.SessionScoped;
 import neuralnetwork.commons.repository.NamedObjectRepository;
 import neuralnetwork.commons.samples.SamplesRepository;
+import neuralnetwork.jpa.entity.SamplesEntity;
+import neuralnetwork.trainerweb.ejb.SamplesFacadeBeanLocal;
 
 /**
  * CDI-managed repository that provides access to the available samples.
@@ -16,19 +20,84 @@ public class SamplesRepositoryBean
         implements Serializable {
     private static final long serialVersionUID = -5990089220125627257L;
     
+    @EJB
+    private SamplesFacadeBeanLocal samplesEJBfacade;
+            
     public SamplesRepositoryBean() {
-        SamplesRepository<Double> repo1 = new SamplesRepository<>();
-        repo1.add(Arrays.asList(0.1, 4.9, -2.3, 5.0));
-        repo1.add(Arrays.asList(0.7, 2.1, 3.4, 4.0));
-        SamplesRepository<Double> repo2 = new SamplesRepository<>();
-        repo2.add(Arrays.asList(4.0, -2.0, 5.0));
-        repo2.add(Arrays.asList(0.0, 2.0, 4.0));
-        SamplesRepository<Double> repo3 = new SamplesRepository<>();
-        repo3.add(Arrays.asList(0.4, -3.0));
-        repo3.add(Arrays.asList(0.0, 1.0));
-        
-        super.add("Samples 1", repo1);
-        super.add("Samples 2", repo2);
-        super.add("Samples 3", repo3);
+    }
+
+    @Override
+    public List<SamplesRepository<Double>> getObjectsList() {
+        return samplesEJBfacade.findAll();
+    }
+
+    @Override
+    public List<String> getNamesList() {
+        return samplesEJBfacade.findAllNames();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public int size() {
+        return samplesEJBfacade.count();
+    }
+
+    @Override
+    public void rename(String oldName, String newName) {
+        if (oldName == null || newName == null) {
+            throw new NullPointerException("Names cannot be null");
+        }
+        SamplesEntity entity = samplesEJBfacade.findByName(oldName);
+        if (entity == null) {
+            throw new IllegalArgumentException("No repository found for name: " +
+                    oldName);
+        }
+        if (containsName(newName)) {
+            throw new IllegalArgumentException("Repository with name " + newName +
+                    " already exists");
+        }
+        entity.setName(newName);
+        samplesEJBfacade.merge(entity);
+    }
+
+    @Override
+    public boolean containsObject(SamplesRepository<Double> object) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsName(String name) {
+        return samplesEJBfacade.findByName(name) != null;
+    }
+
+    @Override
+    public String getNameForObject(SamplesRepository<Double> object) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SamplesRepository<Double> get(String name) {
+        SamplesEntity entity = samplesEJBfacade.findByName(name);
+        return entity == null ? null : entity.getRepository();
+    }
+
+    @Override
+    public boolean remove(String name) {
+        try {
+            samplesEJBfacade.remove(name);
+        }
+        catch (EJBException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void add(String name, SamplesRepository<Double> object) {
+        samplesEJBfacade.persist(name, object);
     }
 }
